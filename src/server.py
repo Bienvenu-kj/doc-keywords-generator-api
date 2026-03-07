8536dfac79f615614aa2dc9f6c6ebaac2048904e
Bienvenu Kajirifo Luc <bienvenukajirifoluc@gmail.com>
2026-03-07 16:12:32 +0000

    change le nom champ de fichier envoyé par le client

--- src/server.py
+++ src/server.py
@@ -8,12 +8,12 @@ def root():
     return "Salut"
 
 @app.post("/keywords")
-def keywordsGenerating(document: UploadFile):
+def keywordsGenerating(file: UploadFile):
     # Exemple de réponse avec des mots-clés fictifs
     sample_keywords = [
         KeywordApiResponse(term="intelligence artificielle", score=0.95),
         KeywordApiResponse(term="génération de mots-clés", score=0.87),
         KeywordApiResponse(term="API", score=0.78)
     ]
-    response = KeywordsApiResponse(document_name=document.filename, keywords=sample_keywords)
+    response = KeywordsApiResponse(document_name=file.filename, keywords=sample_keywords)
     return response 
