f43efcb104f01937117325db7d89c35136a565fe
Bienvenu-kj <bienvenukajirifoluc@gmail.com>
2026-05-01 17:54:13 +0200

    ajoute un nouveau caractère à nettoyer au préprocesseur `native_cleaner.py`

--- src/infrastructure/prepocessors_adapters/native/native_cleaner.py
+++ src/infrastructure/prepocessors_adapters/native/native_cleaner.py
@@ -5,11 +5,12 @@ from ....Domain.ports.preprocessors.i_cleaner import Cleaner
 class InMemoryNativeCleaner(Cleaner):
     async def clean(self, content:str) -> str:
         return re.sub(
-             pattern=r"[0-9+></.=%{}*^$)(&~@?`\§!|£¨;:,'\"]",
+             pattern=r"[0-9+></.=%{}*^$)(&~@?`\§!|£¨;:,'\"«»…]",
              repl="",
              string=(
                  content
                  .replace("\n", " ")
                  .replace("\t", " ")
                  .replace("\r", " ")
+
              ))
